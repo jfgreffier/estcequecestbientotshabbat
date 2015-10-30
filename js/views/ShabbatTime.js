50bcdf883cc6ef5a03747cf0f8021b611ffaa760
jfgreffier <jfgreffier@users.noreply.github.com>
2015-10-30 17:01:30 +0100

    more messages

--- js/views/ShabbatTime.js
+++ js/views/ShabbatTime.js
@@ -1,6 +1,19 @@
 // Global Backbone
 var app = app || {};
 
+// Localized messages
+var messages = {
+  indays: ["Dans quelques jours"],
+  inhours: ["Dans quelques heures", "aujourd'hui"],
+  inminutes: ["Très bientôt", "Non, mais presque", "Presque", "Pas tout à fait", "Pas encore, mais on va faire comme si"],
+  now: ["Oui!", "Ca y est!", "C'est Shabbat!"]
+};
+
+// Useful snippet
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 app.ShabbatTimeView = Backbone.View.extend({
     el: '#shabbatapp',
     initialize: function() {
@@ -28,18 +41,18 @@ app.ShabbatTimeView = Backbone.View.extend({
         // Display result as a friendly message
         var now = moment();
         if (now.isAfter(start) && now.isBefore(end)) {
-            this.$el.html('<p class="msg">Oui!</p>');
+            this.$el.html('<p class="msg">' + messages.now[getRandomInt(0, messages.now.length)] + '</p>');
         } else if (now.isBefore(start)) {
             var diffDays = start.diff(now, "days");
             var diffHours = start.diff(now, "hours");
 
             // Days, hours, minutes
             if (diffDays > 1) {
-                this.$el.html('<p class="msg" title="' + start.from(now) + '">Dans quelques jours</p>');
+                this.$el.html('<p class="msg" title="' + start.from(now) + '">' + messages.indays[getRandomInt(0, messages.indays.length-1)] + '</p>');
             } else if (diffHours > 1) {
-                this.$el.html('<p class="msg" title="' + start.from(now) + '">Dans quelques heures</p>');
+                this.$el.html('<p class="msg" title="' + start.from(now) + '">' + messages.inhours[getRandomInt(0, messages.inhours.length-1)] + '</p>');
             } else {
-                this.$el.html('<p class="msg" title="' + start.from(now) + '">Très bientôt</p>');
+                this.$el.html('<p class="msg" title="' + start.from(now) + '">' + messages.inminutes[getRandomInt(0, messages.inminutes.length-1)] + '</p>');
             }
         }
 
